5c2f5c1383f39b4cbe956142a35417bbda39c014
Lau Delos <97628988+lauradlsantos@users.noreply.github.com>
2022-12-27 18:59:31 -0300

    Interacción entre todos los componentes
    Se solucionaron los problemas de solapación entre componentes.

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 const shopingCartContainer = document.querySelector('#shopingCartContainer')
 const cardsContainer = document.querySelector('.cards-container');
-
+const productDetailContainer = document.querySelector('#productDetail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleMenuCarrito);
 
 function toggleDesktopMenu (){
 const isshopingCartContainerClosed = shopingCartContainer.classList.contains('inactive');
@@ -18,28 +21,43 @@ if (!isshopingCartContainerClosed){
 desktopMenu.classList.toggle('inactive');
 }
 
-menuHamIcon.addEventListener('click', toggleMobileMenu);
 function toggleMobileMenu (){
 const isshopingCartContainerClosed = shopingCartContainer.classList.contains('inactive');
-
 
     if (!isshopingCartContainerClosed){
     shopingCartContainer.classList.add('inactive')
     } 
 
+    closeProductDetailAside();
+
     mobileMenu.classList.toggle('inactive');
 }
 
-menuCarritoIcon.addEventListener('click', toggleMenuCarrito);
 function toggleMenuCarrito (){
-const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
 
-if (!isMobileMenuClosed){
-mobileMenu.classList.add('inactive')
-} 
+    if (!isMobileMenuClosed){
+    mobileMenu.classList.add('inactive')
+    } 
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+    if (!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive')
+    } 
+
 
 shopingCartContainer.classList.toggle('inactive')
 }
+
+function openProductDetailAside(){
+    productDetailContainer.classList.remove('inactive')
+    shopingCartContainer.classList.add('inactive')
+};
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive')
+};
 
 const productList = [];
 productList.push({
@@ -50,12 +68,12 @@ productList.push({
 productList.push({
     name: 'Lamp',
     price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://secure.img1-cg.wfcdn.com/im/78473543/compr-r85/2164/216477710/batesville-64-arched-floor-lamp.jpg'
 })
 productList.push({
     name: 'Computer',
     price: 520,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    image: 'https://i.pcmag.com/imagery/reviews/03vgWGzPszXGuw7ZDpulrMm-1..v1638566437.jpg'
 });
 
 function renderProducts(arr){
@@ -63,11 +81,11 @@ function renderProducts(arr){
         const productCard = document.createElement('div');
         productCard.classList.add('product-card')
     
-    // product = {name, price, img}
-    
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
-    
+        productImg.addEventListener('click', openProductDetailAside)
+        productDetailCloseIcon.addEventListener('click', closeProductDetailAside)
+
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
     
